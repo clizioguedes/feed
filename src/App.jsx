b1af82c1c549787b9e4657d95706cc7767c119c1
Clizio Guedes <clizioguedes@ioasys.com.br>
2023-06-09 18:16:04 -0300

    feature: adding sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Header } from "./components/Header/Header";
+
+import styles from "./App.module.css";
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <h1>Hello World</h1>
-    </>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { PencilLine } from "phosphor-react";
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1536859355448-76f92ebdc33d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=50"
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=Clizio+Guedes"
+        />
+        <strong>Clizio Guedes</strong>
+        <span>Frontend Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
