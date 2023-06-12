ae92a78ce0a9dc714c2f9281246b310869034371
Clizio Guedes <clizioguedes@ioasys.com.br>
2023-06-12 15:04:43 -0300

    feature: adding comments on app

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,117 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/filipedeschamps.png",
+      name: "Filipe Deschamps",
+      role: "Educator",
+    },
+    content: [
+      {
+        id: 1,
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        id: 2,
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        id: 3,
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-06-10 08:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/clizioguedes.png",
+      name: "Clizio Guedes",
+      role: "Frontend developer",
+    },
+    content: [
+      {
+        id: 1,
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        id: 2,
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        id: 3,
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-06-10 09:00:00"),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      {
+        id: 1,
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        id: 2,
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        id: 3,
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-06-10 10:00:00"),
+  },
+  {
+    id: 4,
+    author: {
+      avatarUrl: "https://github.com/akitaonrails.png",
+      name: "Fabio Akita",
+      role: "Co-founder Codeminer 42",
+    },
+    content: [
+      {
+        id: 1,
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        id: 2,
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        id: 3,
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-06-10 11:00:00"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -14,7 +125,17 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                id={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
